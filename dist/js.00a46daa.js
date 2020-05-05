79f1195d227cc981bee999797379f10dcf2af4ab
Fernando Montero <fernando.montero.perez@gmail.com>
2020-05-05 12:58:48 +0100

    PairProgramming: Improved landing page style. Added content

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -35950,7 +35950,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50084" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53598" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
